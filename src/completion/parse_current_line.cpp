5d73b78808b089c02d9b73f478992fbb98cabee9
Christophe Guebert <christophe.guebert@gmail.com>
2020-03-28 11:32:01 +0100

    Extract simple member function & variable.

--- src/completion/parse_current_line.cpp
+++ src/completion/parse_current_line.cpp
@@ -19,17 +19,42 @@ namespace lac::pos
 		if (pos >= view.size() || !isName(view[pos]))
 			return {};
 
+		auto pe = pos, ps = pos;
+
+		auto goToNameStart = [&ps, &view] {
+			while (ps != 0 && isName(view[ps - 1]))
+				--ps;
+		};
+
+		auto ignoreWhiteSpace = [&ps, &view] {
+			while (ps != 0 && std::isspace(view[ps - 1]))
+				--ps;
+		};
+
 		// Go to the end of the name
-		auto pe = pos;
 		while (pe < view.size() && isName(view[pe]))
 			++pe;
 
 		// Go to the start of the name
-		auto ps = pos;
-		while (ps != 0 && isName(view[ps - 1]))
-			--ps;
+		goToNameStart();
 
-		return view.substr(ps, pe - ps);
+		// Cannot go to the left
+		if (ps < 2)
+			return view.substr(ps, pe - ps);
+
+		// Test the next character to the left and see if we must continue
+		pos = ps;
+		ignoreWhiteSpace();
+		auto c = view[ps-1];
+		if (c == '.' || c == ':')
+		{
+			ps -= 2;
+			ignoreWhiteSpace();
+			goToNameStart();
+			pos = ps;
+		}
+
+		return view.substr(pos, pe - pos);
 	}
 
 	boost::optional<ast::Variable> parseVariableAtPos(std::string_view view, size_t pos)
@@ -56,6 +81,8 @@ namespace lac::pos
 		CHECK(extractVariableAtPos("foobar test", 7) == "test");
 		CHECK(extractVariableAtPos("foobar test", 10) == "test");
 		CHECK(extractVariableAtPos("foobar test", 11) == "");
+		CHECK(extractVariableAtPos("foo.bar", 2) == "foo");
+		CHECK(extractVariableAtPos("foo.bar test", 10) == "test");
 	}
 
 	TEST_CASE("Parse name")
@@ -64,6 +91,22 @@ namespace lac::pos
 		REQUIRE(var.has_value());
 		REQUIRE(var->start.get().type() == typeid(std::string));
 		CHECK(boost::get<std::string>(var->start) == "foobar");
+	}
+
+	TEST_CASE("Member variable")
+	{
+		CHECK(extractVariableAtPos("foo.bar", 5) == "foo.bar");
+		CHECK(extractVariableAtPos("foo.bar test", 5) == "foo.bar");
+		CHECK(extractVariableAtPos("foo . bar test", 7) == "foo . bar");
+		CHECK(extractVariableAtPos("test foo.bar", 10) == "foo.bar");
+	}
+
+	TEST_CASE("Member function")
+	{
+		CHECK(extractVariableAtPos("foo:bar", 5) == "foo:bar");
+		CHECK(extractVariableAtPos("foo:bar test", 5) == "foo:bar");
+		CHECK(extractVariableAtPos("foo : bar test", 7) == "foo : bar");
+		CHECK(extractVariableAtPos("test foo:bar", 10) == "foo:bar");
 	}
 
 	TEST_SUITE_END();
