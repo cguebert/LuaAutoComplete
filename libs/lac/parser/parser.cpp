06d1a581c21ac45ad033d65e28dbf9c1f5e80607
Ousmane NDIAYE <o.ndiaye@e-media.fr>
2022-09-13 09:42:01 +0200

    manage some doctest config

--- libs/lac/parser/parser.cpp
+++ libs/lac/parser/parser.cpp
@@ -2,10 +2,6 @@
 #include <lac/parser/parser.h>
 #include <lac/parser/positions.h>
 
-#define DOCTEST_CONFIG_IMPLEMENTATION_IN_DLL
-#define DOCTEST_CONFIG_IMPLEMENT
-#include <doctest/doctest.h>
-
 namespace lac::parser
 {
 	ParseBlockResults::ParseBlockResults(std::string_view view)
